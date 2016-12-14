bbc459b1a7bef6a548f6e640784a24be682a15d3
simonpatrat <simon.patrat@gmail.com>
2016-12-14 08:17:42 -0500

    Updated Projects gallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+$('.project-gallery-item').on('click', function() {
+
+  $('.loader-overlay').css({
+
+  });
+
+  $('.loader-overlay').addClass('visible');
+  setTimeout(function() {
+    $('.loader-overlay').removeClass('visible');
+  }, 3000);
+});
