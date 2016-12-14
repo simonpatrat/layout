67ba34dad8b6b4bdbc0b1ad759679115a51a6ded
simonpatrat <spatrat@kaliop.ca>
2016-12-14 15:02:06 -0500

    Update overlay anim test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,36 +2,18 @@ console.log('\'Allo \'Allo!');
 
 $('.project-gallery-item').on('click', function() {
 
-  var x = $(this).offset().left;
-  var y = $(this).offset().top;
-  var width = $(this).width();
-  var height = $(this).height();
 
-  transformOverlay( function() {
+    $(this).addClass('scaled-up');
 
-     console.log('callback function !');
     $('.loader-overlay').addClass('visible');
+
     setTimeout(function() {
-      $('.loader-overlay').removeClass('visible');
+
+        $('.scaled-up').removeClass('scaled-up');
+        $('.loader-overlay').removeClass('visible');
+
     }, 3000);
-  });
-
-  function transformOverlay(callBack) {
-
-    var overlayWidth = width;
-    var overlayHeight = height;
-    $('.loader-overlay').css({
-      'top' : y + 'px',
-      'left' : x + 'px',
-      'right' : 'auto',
-      'bottom': 'auto',
-      'width' : width + 'px',
-      'height' : height + 'px'
-    });
-
-    return callBack();
-  }
-
+    
 
 
 });
